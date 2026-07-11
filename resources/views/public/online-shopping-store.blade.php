444868be50dc848d6025881a323ad3feb06bcf41
Shahzada shoaib <shahzada.shoaib011@gmail.com>
2026-07-11 14:08:54 +0500

    updates

--- resources/views/public/online-shopping-store.blade.php
+++ resources/views/public/online-shopping-store.blade.php
@@ -742,16 +742,10 @@
                         <button type="button" class="os-filter-scroll-btn os-filter-scroll-prev" aria-label="Scroll filters left">&#8249;</button>
                         <div id="osSubFilters" class="os-sub-filters">
                         <button type="button" class="filter-tag-btn" data-tags="ehed,gold_rings,diamond_rings">Rings</button>
-
-                
                         <button type="button" class="filter-tag-btn" data-tags="gold_earrings,diamond_earrings,gold_tops">Earrings</button>
-
                         <button type="button" class="filter-tag-btn" data-tags="gold_pendants,diamond_pendants,gold_chains">Pendants</button>
-                        <!-- <button type="button" class="filter-tag-btn" data-tags="gold_bangles">BANGLES</button> -->
                         <button type="button" class="filter-tag-btn" data-tags="gold_bracelets,gold_bangles">Bracelets</button>
-                        <!-- <button type="button" class="filter-tag-btn" data-tags="diamond_bands">BANDS</button> -->
                         <button type="button" class="filter-tag-btn" data-tags="mens_rings,ehed">Wedding Bands</button>
-
                         </div>
                         <button type="button" class="os-filter-scroll-btn os-filter-scroll-next" aria-label="Scroll filters right">&#8250;</button>
                     </div>
@@ -763,9 +757,7 @@
                         <span class="os-sort-filter-label">Sort &amp; Filter</span>
                     </button>
                 </div>
-                <!-- <div style="display:flex;justify-content:center;width:100%;margin-top:4px;">
-                    <span style="display:block;height:2px;width:70vw;background:#e6ded3;"></span>
-                </div> -->
+               
             </div>
             <!-- SORT & FILTER OFFCANVAS -->
             <div id="osFilterBarSpacer" class="os-filter-bar-spacer" aria-hidden="true"></div>
